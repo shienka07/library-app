0c12fed5866dde9cd03bda65e4faf75551301752
JO KYEONGHYE <shienka07.ko@gmail.com>
2023-07-19 00:09:13 +0900

    Spring Data JPA를 활용해 save, update, findAll 하기

--- src/main/java/com/group/libraryapp/config/UserConfiguration.java
+++ src/main/java/com/group/libraryapp/config/UserConfiguration.java
@@ -1,7 +1,7 @@
 package com.group.libraryapp.config;
 
 
-import com.group.libraryapp.repository.user.UserRepository;
+import com.group.libraryapp.repository.user.UserJdbcRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,8 +10,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class UserConfiguration {
 
     @Bean
-    public UserRepository userRepository(JdbcTemplate jdbcTemplate) {
-        return new UserRepository(jdbcTemplate);
+    public UserJdbcRepository userRepository(JdbcTemplate jdbcTemplate) {
+        return new UserJdbcRepository(jdbcTemplate);
     }
 
 }
