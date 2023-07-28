42d461d9b9516275aa9f4ab46b6611299c41cf1f
JO KYEONGHYE <shienka07.ko@gmail.com>
2023-07-28 23:07:37 +0900

    책 생성 API 개발하기

--- src/main/java/com/group/libraryapp/service/book/BookService.java
+++ src/main/java/com/group/libraryapp/service/book/BookService.java
@@ -1,20 +1,23 @@
-//package com.group.libraryapp.service.book;
-//
-//import com.group.libraryapp.repository.book.BookMemoryRepository;
-//import com.group.libraryapp.repository.book.BookMySqlRepository;
-//import com.group.libraryapp.repository.book.BookRepository;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class BookService {
-//
-//    private final BookRepository bookRepository;
-//
-//    public BookService(BookRepository bookRepository) {
-//        this.bookRepository = bookRepository;
-//    }
-//
-//    public void saveBook() {
-//        bookRepository.saveBook();
-//    }
-//}
+package com.group.libraryapp.service.book;
+
+import com.group.libraryapp.domain.book.Book;
+import com.group.libraryapp.domain.book.BookRepository;
+import com.group.libraryapp.dto.book.request.BookCreateRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Transactional
+    public void saveBook(BookCreateRequest request) {
+        bookRepository.save(new Book(request.getName()));
+    }
+}
